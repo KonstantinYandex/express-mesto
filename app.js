f09cac6574d42c2d93640cd5784e1e8fa6131b8f
Konstantin <kostka009@yandex.ru>
2022-05-01 13:22:33 +0300

    push

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ const NotFoundError = require("./errors/not-found-error");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose.connect("mongodb://localhost:27017/mestodb ", {
+  useNewUrlParser: true,
+});
+
 app.post(
   "/signin",
   celebrate({
@@ -40,26 +44,22 @@ app.post(
   addUser
 );
 
-app.use("*", () => {
-  throw new NotFoundError("Запрашиваемый ресурс не найден");
-});
-
 app.use(auth);
 
 app.use("/", routerUsers);
 app.use("/", routerCards);
 
-mongoose.connect("mongodb://localhost:27017/mestodb ", {
-  useNewUrlParser: true,
+app.use((req, res, next) => {
+  next(new NotFoundError("Роутер не найден"));
 });
-
-app.use(errors());
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
 
   res.status(statusCode).send({ message });
 });
+
+app.use(errors());
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
