0d951b689863c4b786490f6e923c9cb757340e95
Konstantin <kostka009@yandex.ru>
2022-05-02 22:09:21 +0300

    second

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use((req, res, next) => {
   next(new NotFoundError('Роутер не найден'));
 });
 
-app.use((err, req, res,next) => {
+app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
 
   res.status(statusCode).send({ message });
