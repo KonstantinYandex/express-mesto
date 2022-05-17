55149425e621e60c9e096595851052c88b665a4d
Konstantin <kostka009@yandex.ru>
2022-05-17 12:19:43 +0300

    ffff

--- controllers/user.js
+++ controllers/user.js
@@ -63,7 +63,7 @@ function addUser(req, res, next) {
       password: hash,
     })
       // eslint-disable-next-line no-unused-vars
-      .then((user) => res.status(200).send({
+      .then((user) => res.status(201).send({
         name,
         about,
         avatar,
