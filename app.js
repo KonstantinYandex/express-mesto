41f08c0f2fb4df5b99df85e46d57d48d9775a63d
Konstantin <kostka009@yandex.ru>
2022-05-20 17:14:47 +0300

    ffff

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-require("dotenv").config();
-const { celebrate, Joi, errors } = require("celebrate");
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const routerCards = require("./routers/card");
-const routerUsers = require("./routers/user");
-const auth = require("./middlewares/auth");
-const { login, addUser } = require("./controllers/user");
-const { requestLogger, errorLogger } = require("./middlewares/logger");
+require('dotenv').config();
+const { celebrate, Joi, errors } = require('celebrate');
+const express = require('express');
+// const cors = require("cors");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const routerCards = require('./routers/card');
+const routerUsers = require('./routers/user');
+const auth = require('./middlewares/auth');
+const { login, addUser } = require('./controllers/user');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 const { PORT = 3000 } = process.env;
-const NotFoundError = require("./errors/not-found-error");
+const NotFoundError = require('./errors/not-found-error');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://localhost:27017/mestodb", {
+mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
@@ -27,21 +27,19 @@ mongoose.connect("mongodb://localhost:27017/mestodb", {
 //   'http://localhost:3000',
 // ];
 
-
-
 const cors = (req, res, next) => {
   const { origin } = req.headers;
   console.log(origin);
   const { method } = req;
-  const requestHeaders = req.headers["access-control-request-headers"];
-  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-  res.header("Access-Control-Allow-Origin", origin);
-  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header('Access-Control-Allow-Credentials', true);
 
-  if (method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
-    res.header("Access-Control-Allow-Headers", requestHeaders);
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
 
     return res.end();
   }
@@ -51,27 +49,27 @@ const cors = (req, res, next) => {
 
 app.use(cors);
 
-app.get("/crash-test", () => {
+app.get('/crash-test', () => {
   setTimeout(() => {
-    throw new Error("Сервер сейчас упадёт");
+    throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
 
 app.use(requestLogger);
 
 app.post(
-  "/signin",
+  '/signin',
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().required().email(),
       password: Joi.string().required(),
     }),
   }),
-  login
+  login,
 );
 
 app.post(
-  "/signup",
+  '/signup',
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().required().email(),
@@ -79,20 +77,20 @@ app.post(
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
       avatar: Joi.string().regex(
-        /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/
+        /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/,
       ),
     }),
   }),
-  addUser
+  addUser,
 );
 
 app.use(auth);
 
-app.use("/", routerUsers);
-app.use("/", routerCards);
+app.use('/', routerUsers);
+app.use('/', routerCards);
 
 app.use((req, res, next) => {
-  next(new NotFoundError("Роутер не найден"));
+  next(new NotFoundError('Роутер не найден'));
 });
 
 app.use(errorLogger);
