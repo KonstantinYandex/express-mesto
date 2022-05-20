977d0be5fffcaa836e1a399f8e270fbb965e69f5
Konstantin <kostka009@yandex.ru>
2022-05-20 17:03:51 +0300

    ffff

--- app.js
+++ app.js
@@ -1,68 +1,75 @@
-require('dotenv').config();
-const { celebrate, Joi, errors } = require('celebrate');
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const routerCards = require('./routers/card');
-const routerUsers = require('./routers/user');
-const auth = require('./middlewares/auth');
-const { login, addUser } = require('./controllers/user');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
+require("dotenv").config();
+const { celebrate, Joi, errors } = require("celebrate");
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const routerCards = require("./routers/card");
+const routerUsers = require("./routers/user");
+const auth = require("./middlewares/auth");
+const { login, addUser } = require("./controllers/user");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const app = express();
 const { PORT = 3000 } = process.env;
-const NotFoundError = require('./errors/not-found-error');
+const NotFoundError = require("./errors/not-found-error");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect("mongodb://localhost:27017/mestodb", {
   useNewUrlParser: true,
 });
 
-const allowedCors = [
-  'http://konstantinnovikov.nomoredomains.xyz',
-  'http://api.konstantinnovikov.nomoredomains.xyz',
-  'http://localhost:3000',
-];
+// const allowedCors = [
+//   'http://konstantinnovikov.nomoredomains.xyz',
+//   'http://api.konstantinnovikov.nomoredomains.xyz',
+//   'http://localhost:3000',
+// ];
 
-app.use(cors({
-  origin: allowedCors,
-}));
+app.use(cors());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-  if (req.method === 'OPTIONS') {
-    res.status(200).send();
-    return;
+const cors = (req, res, next) => {
+  const { origin } = req.headers;
+  console.log(origin);
+  const { method } = req;
+  const requestHeaders = req.headers["access-control-request-headers"];
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+
+  res.header("Access-Control-Allow-Origin", origin);
+  res.header("Access-Control-Allow-Credentials", true);
+
+  if (method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
+    res.header("Access-Control-Allow-Headers", requestHeaders);
+
+    return res.end();
   }
-  next();
-});
 
-app.get('/crash-test', () => {
+  return next();
+};
+
+app.get("/crash-test", () => {
   setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
+    throw new Error("Сервер сейчас упадёт");
   }, 0);
 });
 
 app.use(requestLogger);
 
 app.post(
-  '/signin',
+  "/signin",
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().required().email(),
       password: Joi.string().required(),
     }),
   }),
-  login,
+  login
 );
 
 app.post(
-  '/signup',
+  "/signup",
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().required().email(),
@@ -70,20 +77,20 @@ app.post(
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
       avatar: Joi.string().regex(
-        /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/,
+        /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/
       ),
     }),
   }),
-  addUser,
+  addUser
 );
 
 app.use(auth);
 
-app.use('/', routerUsers);
-app.use('/', routerCards);
+app.use("/", routerUsers);
+app.use("/", routerCards);
 
 app.use((req, res, next) => {
-  next(new NotFoundError('Роутер не найден'));
+  next(new NotFoundError("Роутер не найден"));
 });
 
 app.use(errorLogger);
